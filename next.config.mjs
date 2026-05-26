5bc792c3fd58f5352bcd5e8bdec9256416a77774
Rebecca Jennings <157240153+Rebecca-J7@users.noreply.github.com>
2026-05-25 18:54:44 -0700

    resolving APIs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
-  output: "standalone",  // ← add this
   images: {
     unoptimized: true,
   },
