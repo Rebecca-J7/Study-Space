4156819aa976be2ad783949ec9b2e74b747b5ec2
Rebecca Jennings <157240153+Rebecca-J7@users.noreply.github.com>
2026-05-25 18:29:34 -0700

    resolve ESLint configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
